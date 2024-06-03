e2c98cdd41e24c6570a7de33f19c48bf4dc221c8
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-06-03 14:20:32 +0200

    use s4e prefix in s4e-mac.test.c

--- s4e-mac.test.c
+++ s4e-mac.test.c
@@ -9,25 +9,25 @@ int main() {
   asm("add x3, x4, x5");
 
   // CHECK: 0b 00 00 00 reset_acc
-  asm("reset_acc");
+  asm("s4e.reset_acc");
 
   // CHECK: 0b 03 00 02 get_acc_lo x6
-  asm("get_acc_lo x6");
+  asm("s4e.get_acc_lo x6");
 
   // CHECK: 8b 0c 00 04 get_acc_hi x25
-  asm("get_acc_hi x25");
+  asm("s4e.get_acc_hi x25");
 
   // CHECK: 0b 90 1a 01 macu_32 x21, x17
-  asm("macu_32 x21, x17");
+  asm("s4e.macu_32 x21, x17");
 
   // CHECK: 0b 90 1a 03 macs_32 x21, x17
-  asm("macs_32 x21, x17");
+  asm("s4e.macs_32 x21, x17");
 
   // CHECK: 0b a0 1a 01 macu_64 x21, x17
-  asm("macu_64 x21, x17");
+  asm("s4e.macu_64 x21, x17");
 
   // CHECK: 0b a0 1a 03 macs_64 x21, x17
-  asm("macs_64 x21, x17");
+  asm("s4e.macs_64 x21, x17");
 
   return 42;
 }
